05afb5cd16d8b47665adf2dd2e362e84e6b88fe2
anonymoussoh <soh1991@gmail.com>
2015-06-13 20:57:07 +0900

    Add the ability to save json-formatted bookmark to localStorage

--- options.js
+++ options.js
@@ -1,8 +1,17 @@
 function all_items(treenode){
 console.log(treenode);
 var jsoned = JSON.stringify(treenode[0].children[0].children);
-console.log(jsoned);
+//console.log(jsoned);
 document.getElementById("json_field").value = jsoned;
 }
 
 chrome.bookmarks.getTree(all_items);
+
+function save_localstorage(){
+  var jsoned_bookmark_tree = document.getElementById("json_field").value;
+  console.log(jsoned_bookmark_tree);
+  chrome.storage.local.set({'value':jsoned_bookmark_tree});
+}
+
+
+document.getElementById("save").addEventListener("click",save_localstorage,false);
