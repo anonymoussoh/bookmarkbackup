5e8716cd5833aa4de1b3efa8c117a29caacccbfa
anonymoussoh <soh1991@gmail.com>
2015-06-14 01:39:59 +0900

    Change textarea size

--- options.js
+++ options.js
@@ -1,6 +1,6 @@
 function all_items(treenode){
 console.log(treenode);
-var jsoned = JSON.stringify(treenode[0].children[0].children);
+var jsoned = JSON.stringify(treenode[0].children);
 //console.log(jsoned);
 document.getElementById("json_field").value = jsoned;
 }
